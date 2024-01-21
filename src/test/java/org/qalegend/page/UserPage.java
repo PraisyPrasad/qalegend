4d01c212c6f3f908aada08b5a25ca7dc3e20e16a
PraisyPrasad <praisyprasad123@gmail.com>
2024-01-21 22:59:36 +0530

    initial commit

--- src/test/java/org/qalegend/page/UserPage.java
+++ src/test/java/org/qalegend/page/UserPage.java
@@ -15,10 +15,35 @@ public class UserPage {
     }
     @FindBy(xpath = "//a[@class='btn btn-block btn-primary']")
     WebElement addButtonField;
+    @FindBy(xpath = "//table//tr//td[4][contains(text(), 'shifana123@gmail.com')]")
+    WebElement actualMailIdOfNewlyAddedUser;
+
+    @FindBy(xpath="//a[@class='dropdown-toggle']//span[text()='admin KL']")
+    WebElement usersLoggedUserName;
+
+    @FindBy(xpath = "//div[@class='pull-right']//a[@class='btn btn-default btn-flat']")
+    WebElement signOutButton;
+
+    @FindBy(xpath="//table//tr[1]/td[4]")
+    WebElement loggedUsersEmailID;
 
     public AddUsersPage clickOnAddButton() {
         WebElementUtilty.clickOnElement(addButtonField);
         return new AddUsersPage(driver);
+    }
+    public String getTextOfMailID() {
+        String emailText=WebElementUtilty.getTextFromElement(actualMailIdOfNewlyAddedUser);
+        return emailText;
+    }
+    public  void clickOnUsersLoggedUserName() {
+        WebElementUtilty.clickOnElement(usersLoggedUserName);
+    }
+    public LoginPage clickOnSignOutButton(){
+        WebElementUtilty.clickOnElement(signOutButton);
+        return new LoginPage(driver);
+    }
+    public String getTextFromLoggedUsersMailID() {
+        return WebElementUtilty.getTextFromElement(loggedUsersEmailID);
     }
 }
 
